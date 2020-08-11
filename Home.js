a9eedef39592f6b25152c5070ad701ed8e86ecef
FuJun-Tsai <55325225+FuJun-Tsai@users.noreply.github.com>
2020-08-11 21:42:19 +0800

    20200811

--- Home.js
+++ Home.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
 
     $('.hamburger').click(function(){
 
-        $('.maintitle').toggleClass('maintitle_ctrl');
+        $('.maintitle').toggleClass('maintitle_ctrl').css('backgroundColor','#383A3F');
+
 
     });
-
-
-
 
 });
