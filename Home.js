f6624c5a11914b2db04930f98af438ab9796f188
FuJun-Tsai <55325225+FuJun-Tsai@users.noreply.github.com>
2020-08-12 18:39:59 +0800

    20200812

--- Home.js
+++ Home.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
     $('.hamburger').click(function(){
 
         $('.maintitle').toggleClass('maintitle_ctrl').css('backgroundColor','#383A3F');
-
-
+        
     });
+
+    let bannerheight = $('body').width() * 0.67; 
+    $('.banner').css('height',bannerheight);
 
 });
