79d9f24cb4fe8cf0a7b70008982502b6cff05a07
FuJun-Tsai <55325225+FuJun-Tsai@users.noreply.github.com>
2020-08-15 14:54:40 +0800

    20200815

--- Drink.js
+++ Drink.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
 
     
-    let imgwidth = $('html').width() * 300 / 1903 ;
-
-    if( $('html').width() < 1200){
-    $('.productlayout img').css('width' , imgwidth);
+    // let imgwidth = $('html').width() * 300 / 1903 ;
+    // if( $('html').width() < 1200){
+    // $('.productlayout img').css('width' , imgwidth);
     
-    }
+    // }
+
+    $('.page').click(function(){
+        $('.page').removeClass('here');
+        $('.page').addClass('hide');
+        $(this).removeClass('hide');
+        $(this).addClass('here');
+
+    });
+
+
 
 });
