362748ff8ed10318076fb3fc05b22eda202eba7d
FuJun-Tsai <55325225+FuJun-Tsai@users.noreply.github.com>
2020-08-22 16:38:25 +0800

    20200822

--- Rerserve.js
+++ Rerserve.js
@@ -1,22 +1,83 @@
 $(document).ready(function(){
 
-  let screen = $('html').width();
+  // 訂位桌RWD
 
-  if( screen < 700){
+    $(window).resize(function(){
+  
+      let screen = $('html').width();
+    // screen = 螢幕寬
+      if( screen < 700){
+        
+        let bookingheight = screen * 12 / 7 ;
+
+        $('.result').addClass('-open');
+
+        $('.bookwindow').css('height',bookingheight);
+
+      }
+
+    });
+    // 寬小於700後 直立呈現
+  // /訂位桌RWD
+
+  // 資訊總整方塊
+    $('.btn_drawer').click(function(){
+
+      $('.result').toggleClass('-open');
+
+    });
+  // /資訊總整方塊
+
+  // 資訊總整
+    $('input').blur(function(){
+      let from = $(this).attr('id');
+      let text = $(this).val();
+
+      $(`.${from}`).text(text);
+
+    });
     
-    let bookingheight = screen * 12 / 7 ;
+    $('.bookwindow button').on('click',function(){            
 
-    $('.result').addClass('-open');
+      if($(this).attr('id').length == 1 ){ 
+      let NOid = $(this).attr('id');
 
-    $('.bookwindow').css('height',bookingheight);
+        $(`.${NOid} button`).toggleClass('selected');
+
+      }else{
+        $(this).toggleClass('selected');
+
+      }
+      
+      let from = $(this).attr('id');
+      let picking = $('.x.selected').length;
+
+      // if ($('.tables').text() == ''){
+      //   $('.tables').text(from).append(',');
+      // }else{
+      //   $('.tables').append(from).append(',');
+      // }
+
+      $('.mans').text(picking);
+
+      if (picking>0){
+
+        let desk = new Array;
+        for ( i=0 ; i<picking ; i+=1){
+          let from = $(`.x.selected:nth(${i})`).attr('id');
+          desk[i] = from;
+          $('.tables').text(desk);
+          
+        }
+
+        }else{
+          desk = [];
+          $('.tables').text(desk);
+      }  
+
+    });
 
 
-  }
-
-  $('.btn_drawer').click(function(){
-
-    $('.result').toggleClass('-open');
+      // /資訊總整
 
   });
-
-});
